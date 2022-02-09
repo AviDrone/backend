41313613c46a97f8f54053496944712e00d99c9c
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-02-09 12:49:16 -0800

    Create calibration.py, telemetry.py

---
+++ UAV/calibration.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+from mavsdk import System
+
+# TODO test this with our own drone.
+
+
+async def run():
+    """
+    For more info, see:
+    http://mavsdk-python-docs.s3-website.eu-central-1.amazonaws.com/plugins/calibration.html
+    https://github.com/mavlink/MAVSDK-Python/blob/main/examples/calibration.py
+
+    """
+
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("-- Starting gyroscope calibration")
+    async for progress_data in drone.calibration.calibrate_gyro():
+        print(progress_data)
+    print("-- Gyroscope calibration finished")
+
+    print("-- Starting accelerometer calibration")
+    async for progress_data in drone.calibration.calibrate_accelerometer():
+        print(progress_data)
+    print("-- Accelerometer calibration finished")
+
+    print("-- Starting magnetometer calibration")
+    async for progress_data in drone.calibration.calibrate_magnetometer():
+        print(progress_data)
+    print("-- Magnetometer calibration finished")
+
+    print("-- Starting board level horizon calibration")
+    async for progress_data in drone.calibration.calibrate_level_horizon():
+        print(progress_data)
+    print("-- Board level calibration finished")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
