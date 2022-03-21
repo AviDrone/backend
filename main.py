8f109c81a0362a57896d9e2061c45633ead33434
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-03-21 12:17:21 -0700

    Update cli arguments

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
 
     group.add_argument("--primary", action="store_true", help="primary search only")
     group.add_argument("--secondary", action="store_true", help="secondary search only")
-    parser.add_argument(
-        "-s", "--search", action="store_true", help="start avidrone search mission"
-    )
+    parser.add_argument("-test", action="store_true", help="Testing search")
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="increase output verbosity"
     )
-
+    parser.add_argument(
+        "-search", action="store_true", help="start avidrone search mission"
+    )
     args = parser.parse_args()
 
-    print("\n********\nAVIDRONE\n********\n")
+    print("\n********\nAvidrone Search\n********\n")
     if args.verbose:
         log.info("--Verbose: True")
         parameters.IS_VERBOSE = True
