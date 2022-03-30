0df111644e65465394630c61720bcf9647059266
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-03-30 14:57:31 -0700

    Implement transceiver object

--- avidrone/transceiver/transceiver.py
+++ avidrone/transceiver/transceiver.py
@@ -1,39 +1,40 @@
 class Transceiver:
-    def __init__(self, position, signal_status):
+    def __init__(self, position):
         position = [-1, -1]
         self.min_range = 5
         self.max_range = 27
-        self.signal_status = signal_status
+        self.signal_status = False
         self.position = position # default, transceiver signal not acquired
 
     @staticmethod
-    def signal_status(self, signal_status):
+    def signal_status(self):
         signal_acquired = False
-        signal_detected = signal_status
+        signal_detected = self.signal_status
 
         if signal_detected:
             signal_acquired = True
         return signal_acquired
 
     @staticmethod
-    def bin_direction(self, position, min_range, max_range):
+    def binned_direction(self, position):
         """
         0 = forward
         1 = left
         2 = right
-        """
-        min_val = min_range
-        max_val = max_range
-        
+        """        
         new_direction = -1  # default, error
         direction = position[0]
         distance = position[1]
 
         if direction == 2:
             new_direction = 0
-        elif direction < 2 and distance < min_val:
+        elif direction < 2 and distance < self.min_range:
             new_direction = 1
-        elif direction > 2 and distance < max_val:
+        elif direction > 2 and distance < self.max_range:
             new_direction = 2
 
         return new_direction
+
+    @staticmethod
+    def distance(self, position):
+        pass
