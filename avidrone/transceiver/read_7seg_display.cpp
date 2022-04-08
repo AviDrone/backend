47836c3aa70f60e2eb551bbf10b3105ddead1243
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-04-07 19:44:09 -0700

    Lint cpp

--- avidrone/transceiver/read_7seg_display.cpp
+++ avidrone/transceiver/read_7seg_display.cpp
@@ -1,7 +1,7 @@
 // Copyright 2022 Avidrone
 
 #include "read.h"
-#include "read_7seg_display.h"  // Only runs on Raspberry Pi
+#include "read_7seg_display.h" // Only runs on Raspberry Pi
 #include <fstream>
 #include <iostream>
 
@@ -121,21 +121,21 @@ double dir_decode(bool read_dir[5]) {
 double dist_to_double(char dist[4]) {
   if ((dist[0] == 'x') && (dist[1] == 'x') && (dist[2] == 'x')) { // xxx
     return -1;
-  } else if ((dist[1] == 'x') && (dist[2] == 'x')) {  // rxx
+  } else if ((dist[1] == 'x') && (dist[2] == 'x')) { // rxx
     return -1;
-  } else if ((dist[0] == 'x') && (dist[2] == 'x')) {  // xrx
+  } else if ((dist[0] == 'x') && (dist[2] == 'x')) { // xrx
     return -1;
-  } else if ((dist[0] == 'x') && (dist[1] == 'x')) {  // xxr
+  } else if ((dist[0] == 'x') && (dist[1] == 'x')) { // xxr
     return -1;
-  } else if ((dist[0] != 'x') && (dist[1] == '.') && (dist[2] == 'x')) {  // rrx
+  } else if ((dist[0] != 'x') && (dist[1] == '.') && (dist[2] == 'x')) { // rrx
     return -1;
-  } else if ((dist[0] != 'x') && (dist[1] == 'x') && (dist[2] != 'x')) {  // rxr
+  } else if ((dist[0] != 'x') && (dist[1] == 'x') && (dist[2] != 'x')) { // rxr
     dist[1] = dist[2];
     dist[2] = 'x';
     return atof(dist);
-  } else if ((dist[0] != 'x') && (dist[1] == '.') && (dist[2] != 'x')) {  // rrr
+  } else if ((dist[0] != 'x') && (dist[1] == '.') && (dist[2] != 'x')) { // rrr
     return atof(dist);
-  } else if ((dist[0] == 'x') && (dist[1] == '.') && (dist[2] != 'x')) {  // xrr
+  } else if ((dist[0] == 'x') && (dist[1] == '.') && (dist[2] != 'x')) { // xrr
     dist[0] = '0';
     return atof(dist);
   } else {
@@ -166,8 +166,8 @@ void get_dir_dig(double *dir_dig_out) {
   }
   // Perform operations to determine the reading and filter out bad readings
   for (int i = 0; i < NUM_READS; i++) {
-    all_dig_decode[i][0] = decode_7seg((bool *)(all_dig1_reading[i]));
-    all_dig_decode[i][2] = decode_7seg((bool *)(all_dig2_reading[i]));
+    all_dig_decode[i][0] = decode_7seg(reinterpret_cast<bool *>(all_dig1_reading[i]));
+    all_dig_decode[i][2] = decode_7seg(reinterpret_cast<bool *>(all_dig2_reading[i]));
     all_dig_decode[i][3] = '\0';
     if (all_dp_reading[i] == 0) {
       all_dig_decode[i][1] = '.';
@@ -176,7 +176,7 @@ void get_dir_dig(double *dir_dig_out) {
       all_dig_decode[i][2] = '\0';
     }
     all_dir_dig_out[i][1] = dist_to_double(all_dig_decode[i]);
-    all_dir_dig_out[i][0] = dir_decode((bool *)(all_dir_reading[i]));
+    all_dir_dig_out[i][0] = dir_decode(reinterpret_cast<bool *>(all_dir_reading[i]));
   }
 
   // int dir_counts[6] = {0}; // Directional LEDs 0-4 and then -1 if not read
