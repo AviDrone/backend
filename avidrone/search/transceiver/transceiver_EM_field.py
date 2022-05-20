c978ad5aa105e9b64de71e84e72f0f0891f6c8ab
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-05-20 11:10:17 -0700

    Update

--- avidrone/search/transceiver/transceiver_EM_field.py
+++ avidrone/search/transceiver/transceiver_EM_field.py
@@ -21,17 +21,27 @@ Bamp_1 = np.linalg.norm(B_1, axis=2)
 Bamp_1 /= np.amax(Bamp_1)
 # coil_1.show()
 
+
 # coil 2
 ts = np.linspace(-8, 8, 1000)
 vertices_2 = np.c_[5 * np.cos(ts * 2 * np.pi), 5 * np.sin(ts * 2 * np.pi), ts]
 coil_2 = magpy.current.Line(current=20, vertices=vertices_2)
 coil_2.rotate_from_angax(-45, "y")
 
-# # compute and plot field of coil 2
+# compute and plot field of coil 2
 B_2 = magpy.getB(coil_2, grid)
 Bamp_2 = np.linalg.norm(B_2, axis=2)
 Bamp_2 /= np.amax(Bamp_2)
-# coil_2.show()
+# # coil_2.show()
+
+# superposition principle of magnetic field
+# Reference: http://www.physicsbootcamp.org/Superposition-of-Magnetic-Fields.html
+
+# plotting magnetic fields of coil 1 and coil 2
+B = np.add(B_1, B_2)
+B_amp = np.linalg.norm(B, axis=2)
+B_amp /= np.amax(B_amp)
+
 
 # Plot
 fig, [ax1, ax2] = plt.subplots(1, 2, figsize=(9, 4))
@@ -47,14 +57,27 @@ sp_1 = ax1.streamplot(
     cmap="coolwarm",
 )
 
+# coil 2 plot
+# sp_2 = ax2.streamplot(
+#     grid[:, :, 0],
+#     grid[:, :, 2],
+#     B_2[:, :, 0],
+#     B_2[:, :, 2],
+#     density=3,
+#     color=Bamp_2,
+#     linewidth=np.sqrt(Bamp_2) * 2,
+#     cmap="coolwarm",
+# )
+
+# combined coils plot
 sp_2 = ax2.streamplot(
     grid[:, :, 0],
     grid[:, :, 2],
-    B_2[:, :, 0],
-    B_2[:, :, 2],
+    B[:, :, 0],
+    B[:, :, 2],
     density=3,
-    color=Bamp_2,
-    linewidth=np.sqrt(Bamp_2) * 2,
+    color=B_amp,
+    linewidth=np.sqrt(B_amp) * 2,
     cmap="coolwarm",
 )
 
@@ -66,7 +89,7 @@ ax1.set(
     aspect=1,
 )
 ax2.set(
-    title="Magnetic field of coil 2",
+    title="Combined magnetic field of coil 1 and coil 2",
     xlabel="x-position [mm]",
     ylabel="z-position [mm]",
     aspect=1,
