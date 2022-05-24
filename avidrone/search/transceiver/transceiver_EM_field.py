7b407c14ddf7a1621f2299dc9ce86d0859f742da
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-05-24 00:27:51 -0700

    Cfreate setup.py

--- avidrone/search/transceiver/transceiver_EM_field.py
+++ avidrone/search/transceiver/transceiver_EM_field.py
@@ -8,15 +8,17 @@ import numpy as np
 # https://magpylib.readthedocs.io/en/latest/examples/examples_30_coil_field_lines.html
 """
 
+#   TODO Create a unique plot for combined EM field
+
 # create grid
-ts = np.linspace(-100, 100, 100)
+ts = np.linspace(-200,200, 200)
 grid = np.array([[(x, 0, z) for x in ts] for z in ts])
 
 # coil 1
-ts = np.linspace(-8, 8, 1000)
+ts = np.linspace(-60, 60, 1000)
 vertices_1 = np.c_[5 * np.cos(ts * 2 * np.pi), 5 * np.sin(ts * 2 * np.pi), ts]
 
-coil_1 = magpy.current.Line(current=20, vertices=vertices_1)
+coil_1 = magpy.current.Line(current=750, vertices=vertices_1)    # AAA battery
 coil_1.rotate_from_angax(45, "y")
 
 # compute and plot field of coil 1
@@ -26,16 +28,16 @@ Bamp_1 /= np.amax(Bamp_1)
 # coil_1.show()
 
 # coil 2
-ts = np.linspace(-8, 8, 1000)
+ts = np.linspace(-60, 60, 1000)
 vertices_2 = np.c_[5 * np.cos(ts * 2 * np.pi), 5 * np.sin(ts * 2 * np.pi), ts]
-coil_2 = magpy.current.Line(current=20, vertices=vertices_2)
+coil_2 = magpy.current.Line(current=750, vertices=vertices_2)
 coil_2.rotate_from_angax(-45, "y")
 
 # compute and plot field of coil 2
 B_2 = magpy.getB(coil_2, grid)
 Bamp_2 = np.linalg.norm(B_2, axis=2)
 Bamp_2 /= np.amax(Bamp_2)
-# # coil_2.show()
+# coil_2.show()
 
 # plotting magnetic fields of coil 1 and coil 2
 B = np.add(B_1, B_2)
@@ -56,17 +58,17 @@ sp_1 = ax1.streamplot(
     cmap="coolwarm",
 )
 
-# coil 2 plot
-sp_2 = ax2.streamplot(
-    grid[:, :, 0],
-    grid[:, :, 2],
-    B_2[:, :, 0],
-    B_2[:, :, 2],
-    density=3,
-    color=Bamp_2,
-    linewidth=np.sqrt(Bamp_2) * 2,
-    cmap="coolwarm",
-)
+# # coil 2 plot
+# sp_2 = ax2.streamplot(
+#     grid[:, :, 0],
+#     grid[:, :, 2],
+#     B_2[:, :, 0],
+#     B_2[:, :, 2],
+#     density=3,
+#     color=Bamp_2,
+#     linewidth=np.sqrt(Bamp_2) * 2,
+#     cmap="coolwarm",
+# )
 
 # combined coils plot
 sp_combined = ax2.streamplot(
@@ -95,23 +97,24 @@ def get_theta_grid():
     return test
 
 
-# figure styling
-ax1.set(
-    title="Magnetic field of coil 1",
-    xlabel="x-position [mm]",
-    ylabel="z-position [mm]",
-    aspect=1,
-)
+# # figure styling
+# ax1.set(
+#     title="Magnetic field of front coil",
+#     xlabel="x-position [mm]",
+#     ylabel="z-position [mm]",
+#     aspect=1,
+# )
+
 ax2.set(
-    title="Combined magnetic field of coil 1 and coil 2",
+    title="Magnetic field of a two-antenna transceiver",
     xlabel="x-position [mm]",
     ylabel="z-position [mm]",
     aspect=1,
 )
 
 plt.colorbar(sp_1.lines, ax=ax1, label="[mT]")
-# plt.colorbar(sp_2, ax=ax2, label="[mT]")
+# plt.colorbar(sp_2.lines, ax=ax2, label="[mT]")
 plt.colorbar(sp_combined.lines, ax=ax2, label="[mT]")
 
 plt.tight_layout()
-# plt.show()
+plt.show()
