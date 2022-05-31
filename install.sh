aa5df917c6d560ae384afd8c73071a3365e2f42f
samuelShernandez <samuel.hernandez@wallawalla.edu>
2022-05-31 00:07:46 -0700

    Documentation

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ apt-get update              # To get the latest package lists
 apt-get upgrade
 apt install pipenv -y       # pipenv virtual environment
 pipenv install
+pip install -r requirements.txt
