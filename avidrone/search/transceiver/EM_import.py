088e026ad14c1a822c63e3b66a250e653a13d5c1
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-06-05 15:47:40 -0700

    lint

--- avidrone/search/transceiver/EM_import.py
+++ avidrone/search/transceiver/EM_import.py
@@ -1,7 +1,7 @@
-from dataclasses import astuple
+import utm
 from EM_field import EM_field
 from util import get_direction
-import utm
+
 
 home = [46.0452822, -118.3930353, 584]  # test: where the beacon is
 rel_pos = [40, 20, 24]  # test: where the uav is
@@ -15,26 +15,18 @@ latlon2utm = utm.from_latlon(home[0], home[1])
 utm_pos = []
 for var in latlon2utm:
     utm_pos.append(var)
-    print(utm_pos[latlon2utm.index(var)])
+
 
 def add_rel_pos(utm_pos, rel_pos):
-    new_ = []    
+    new_ = []
     new_.append(utm_pos[0] + rel_pos[0])
     new_.append(utm_pos[1] + rel_pos[1])
     new_.append(utm_pos[2])
     new_.append(utm_pos[3])
     return new_
 
+
 new_utm = add_rel_pos(utm_pos, rel_pos)
 utm2latlon = utm.to_latlon(new_utm[0], new_utm[1], new_utm[2], new_utm[3])
+print(latlon2utm)
 print(utm2latlon)
-
-
-
-def print_result(theta, direction):
-    print(f"flux direction, output direction: {int(theta)}, {direction}")
-
-
-# test = EM_field.get_rel2abs_pos(test_field, rel_pos)
-# print_result(theta, direction)
-# print(test)
