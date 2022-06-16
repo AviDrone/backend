8ab8345bc1c64f7bc88bed3580a721c3866495ff
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-06-15 22:39:01 -0500

    test parameters

--- test/search/test_parameters.py
+++ test/search/test_parameters.py
@@ -3,19 +3,18 @@
 
 import unittest
 
-from app.search import parameters
+from app.search import parameters as p
 
 
 class TestParameters(unittest.TestCase):
     def __init__(self):
-        self.MISSION_TIMEOUT = False
-        self.ALTITUDE = 15
-        self.DEGREES = 10
-        self.DEGREE_ERROR = 2
-        self.DISTANCE_ERROR = 0.35
-        self.LAND_THRESHOLD = 2
-        self.MAGNITUDE = 1
-        self.WINDOW_SIZE = 5
+        self.T_ALTITUDE = 15
+        self.T_DEGREES = 10
+        self.T_DEGREE_ERROR = 2
+        self.T_DISTANCE_ERROR = 0.35
+        self.T_LAND_THRESHOLD = 2
+        self.T_MAGNITUDE = 1
+        self.T_WINDOW_SIZE = 5
 
     def test_parameters(self):
         altitude_msg = "Incorrect altitude!"
@@ -26,18 +25,18 @@ class TestParameters(unittest.TestCase):
         magnitude_msg = "Incorrect magnitude!"
         window_size_msg = "Incorrect window size!"
 
-        self.assertFalse(parameters.MISSION_TIMEOUT)
-        self.assertEquals(self.ALTITUDE, parameters.ALTITUDE, altitude_msg)
-        self.assertEquals(self.DEGREES, parameters.DEGREES, degrees_msg)
-        self.assertEquals(self.DEGREE_ERROR, parameters.DEGREE_ERROR, degree_error_msg)
+        self.assertFalse(p.MISSION_TIMEOUT)
+        self.assertEquals(self.T_ALTITUDE, p.ALTITUDE, altitude_msg)
+        self.assertEquals(self.T_DEGREES, p.DEGREES, degrees_msg)
+        self.assertEquals(self.T_DEGREE_ERROR, p.DEGREE_ERROR, degree_error_msg)
         self.assertEquals(
-            self.DISTANCE_ERROR, parameters.DISTANCE_ERROR, distance_error_msg
+            self.T_DISTANCE_ERROR, p.DISTANCE_ERROR, distance_error_msg
         )
         self.assertEquals(
-            self.LAND_THRESHOLD, parameters.AND_THRESHOLD, land_threshold_msg
+            self.T_LAND_THRESHOLD, p.AND_THRESHOLD, land_threshold_msg
         )
-        self.assertEquals(self.MAGNITUDE, parameters.MAGNITUDE, magnitude_msg)
-        self.assertEquals(self.WINDOW_SIZE, parameters.WINDOW_SIZE, window_size_msg)
+        self.assertEquals(self.T_MAGNITUDE, p.MAGNITUDE, magnitude_msg)
+        self.assertEquals(self.T_WINDOW_SIZE, p.WINDOW_SIZE, window_size_msg)
 
 
 if __name__ == "__main__":
